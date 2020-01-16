8a9abe43764e8c1c96cc9b63491456f4aabbbf49
ascorbic <ascorbic@gmail.com>
2020-01-16 19:15:32 +0000

    Change name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Contentful starter',
+    title: 'TRexible',
   },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
