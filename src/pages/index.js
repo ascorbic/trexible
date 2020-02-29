0388d438fee902b34b9f3b90bff3768782403bc7
ascorbic <ascorbic@gmail.com>
2020-02-29 16:23:31 +0000

    Remove tracesvg

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export const pageQuery = graphql`
           tags
           heroImage {
             fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-             ...GatsbyContentfulFluid_tracedSVG
+             ...GatsbyContentfulFluid
             }
           }
           description {
@@ -78,7 +78,7 @@ export const pageQuery = graphql`
               resizingBehavior: PAD
               background: "rgb:000000"
             ) {
-              ...GatsbyContentfulFluid_tracedSVG
+              ...GatsbyContentfulFluid
             }
           }
         }
